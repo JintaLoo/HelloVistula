449ca574981231ac5354b650f5583aed5b4a0b12
Kanisorn <kjintaa1@stu.vistula.edu.pl>
2022-03-04 18:03:18 +0100

    Test

---
+++ src/HelloVistula.java
@@ -0,0 +1,8 @@
+public class HelloVistula
+        {
+            public static void main(String[] args)
+            {
+                System.out.print("Hello Vistula");
+            }
+
+        }
